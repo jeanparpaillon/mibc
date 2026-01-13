07488ef0790e5e0fca122fcfa115a849ba97a629
Jean Parpaillon <jean@parpaillon.info>
2026-01-13 15:00:56 +0100

    Make app standalone

--- mix.exs
+++ mix.exs
@@ -5,10 +5,6 @@ defmodule Mibc.MixProject do
     [
       app: :mibc,
       version: "0.1.0",
-      build_path: "../../_build",
-      config_path: "../../config/config.exs",
-      deps_path: "../../deps",
-      lockfile: "../../mix.lock",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: []
